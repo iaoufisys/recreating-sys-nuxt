774463b308296ff51a3528dfaf2bcfe8fa979ce1
iaoufisys <iaoufi@seeyousun.fr>
2024-06-21 09:24:33 +0200

    wip

--- app.vue
+++ app.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <!-- <MainPageSYS/> -->
     <LandingPage />
   </div>
 </template>
 
 <script setup>
-// import NavBar from "./components/NavBar.vue"
 import LandingPage from "./components/LandingPage.vue"
 // import MainPageSYS from "./components/MainPageSYS.vue"
 
@@ -14,4 +12,10 @@ import LandingPage from "./components/LandingPage.vue"
 
 <style>
   @import './public/style.css';
+
+*{
+  margin:0;
+  padding: 0;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 </style>
--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,21 +1,125 @@
 <template>
-  <h3>landing page</h3>
-  <div>
-    <NuxtImg
-      v-if="page"
-      :src="
-        '/backend' + page.data.attributes.PageContent[0].imgBg.data.attributes.url
-      "/>
-    <!-- <div class="bgImg"
-      v-bind:style="{ 'background-image': 'url(' + api_url + page.data.attributes.PageContent[0].imgBg.data.attributes.url + ')' }">
-    </div> -->
-    <h1 v-if="page">{{ page.data.attributes.PageContent[0].titre }}</h1>
-    <div v-if="page" class="bgColor">
+<div class="bigcont" v-if="page">
+  
+  <div class="sidebar">
+    <div class="nav-bar">
       <div>
-        <h2>{{ page.data.attributes.PageContent[0].titre }}</h2>
+        <img src="../public/assets/logo.png" alt="">
+      </div>
+      <hr class="long-hr">
+      <a href="">YELLO PADEL</a>
+      <hr>
+      <a href="">LE PADEL</a>
+      <hr>
+      <a href="">AVANTAGES</a>
+      <hr>
+      <a href="">CONTACT</a>
+    </div>
+  </div>
+
+<div>
+  <header>
+    <NuxtImg
+      :src="'/backend' + page.data.attributes.PageContent[0].imgBg.data.attributes.url"
+      class="bg-img"/>
+    <div>
+      <h1>{{ page.data.attributes.PageContent[0].titre[0].children[0].text }} <span>{{page.data.attributes.PageContent[0].titre[0].children[1].text}}</span> </h1>
+      <hr class="long-hr">
+    </div>
+  </header>
+
+  <section class="bgColor">
+    <div class="container">
+      <h2 class="title-relative text-center">{{ page.data.attributes.PageContent[1].titre }}</h2>
+      <div class="strapi-blocks">
+        <StrapiBlocks :content="page.data.attributes.PageContent[1].Texte"/>
+      </div>
+    </div>
+  </section>
+
+  <section class="noBg">
+    <div class="container">
+      <h2 class="title-relative">{{ page.data.attributes.PageContent[2].titre }}</h2>
+      <hr>
+      <div class="strapi-blocks">
+        <StrapiBlocks :content="page.data.attributes.PageContent[2].Texte"/>
+      </div>
+    </div>
+  </section>
+
+  <section class="transpBg">
+    <div class="container">
+      <div>
+        <h2 class="title-relative">{{ page.data.attributes.PageContent[3].titre }}</h2>
+        <hr>
+        <div class="strapi-blocks">
+          <StrapiBlocks :content="page.data.attributes.PageContent[3].Texte"/>
+        </div>
+      </div>
+      <div class="rotate-45">
+        <NuxtImg
+        class="img"
+        :src="'/backend' + page.data.attributes.PageContent[3].image.data.attributes.url"/>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <h3> {{page.data.attributes.PageContent[4].Texte}} </h3>
+    </div>
+      <hr class="hr2">
+  </section>
+
+  <section class="pistesBg">
+    <h4> {{page.data.attributes.PageContent[5].NombresPistes}} </h4>
+    <p>Pistes de padel</p>
+  </section>
+
+  <section class="articles">
+    <h2>Ils parlent de nous</h2>
+    <div>
+      <div>
+        <NuxtImg
+        class="img-articles"
+        :src="'/backend' + page.data.attributes.PageContent[6].article[0].img.data.attributes.url"/>
+        <h3> {{ page.data.attributes.PageContent[6].article[0].Titre }} </h3>
+        <button ><a :href='page.data.attributes.PageContent[6].article[0].lien'>Lire l'article</a></button>
+      </div>
+      <div>
+        <NuxtImg
+        class="img-articles"
+        :src="'/backend' + page.data.attributes.PageContent[6].article[1].img.data.attributes.url"/>
+        <h3> {{ page.data.attributes.PageContent[6].article[1].Titre }} </h3>
+        <button ><a :href='page.data.attributes.PageContent[6].article[1].lien'>Lire l'article</a></button>
+      </div>
+      <div>
+        <NuxtImg
+        class="img-articles"
+        :src="'/backend' + page.data.attributes.PageContent[6].article[2].img.data.attributes.url"/>
+        <h3> {{ page.data.attributes.PageContent[6].article[2].Titre }} </h3>
+        <button ><a :href='page.data.attributes.PageContent[6].article[2].lien'>Lire l'article</a></button>
+      </div>
+    </div>
+  </section>
+
+  <div class="footer">
+    <div>
+      <div>
+        <NuxtImg  
+        :src="'/backend' + page.data.attributes.PageContent[7].imgBg.data.attributes.url"
+        class="footer-img"/>
+      </div>
+      <div class="footer-cont">
+        <h5>Contactez-nous</h5>
+        <p>{{ page.data.attributes.PageContent[7].mail }}</p>
+        <p>{{ page.data.attributes.PageContent[7].adresse }}</p>
       </div>
     </div>
   </div>
+
+</div>
+</div>
 </template>
 
 <script setup>
@@ -30,28 +134,57 @@ const { data: page } = await useFetch(
 </script>
 
 <style>
-NuxtImg {
-  width: 100vw;
-}
-body {
-  padding: 0;
-  margin: 0;
-}
-.bgImg {
-  height: 500px;
-  display: flex;
-  align-items: center;
+@import '../public/style.css';
+*{
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
-.bgColor {
-  margin: 50px 0;
-  padding: 30px 0;
+.sidebar{
   display: flex;
-  background-color: darksalmon;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #111827;
+  padding: 1em;
+  width: 40%;
   align-items: center;
 }
-h1 h2 {
-  text-align: center;
+.nav-bar{
+  position: fixed;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  overflow:hidden;
+  width: 15%;
+  background-color: #111827;
+}
+.nav-bar>hr{
+  border-color: white;
+  border-width: 1px;
+  margin : 0;
+  width: 60%;
+}
+.nav-bar>.long-hr{
+  width: 50%;
+  margin: 0 auto;
+}
+.nav-bar>a{
+  color: white;
+  width: fit-content
+}
+.nav-bar>a:hover{
+  font-size: 2em;
+  color: #FDDC97;
+}
+.nav-bar>div{
+  width: 90%;
+  margin: 0 auto;
+}
+img{
+  width: 100%;
+}
+button{
+  cursor: pointer;
 }
 </style>
